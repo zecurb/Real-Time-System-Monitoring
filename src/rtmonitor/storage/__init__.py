5a9f20f0a18fae39280b156e1cc488a2e1726f0b
zecurb <nehemiahboyce@gmail.com>
2026-07-30 03:42:46 -0400

    feat: add normalized time-series storage

--- src/rtmonitor/storage/__init__.py
+++ src/rtmonitor/storage/__init__.py
@@ -1,12 +1,20 @@
 """Durable telemetry storage."""
 
-from rtmonitor.storage.base import EventStore, QueueLease, QueueStats, QueueStatus, StoreResult
+from rtmonitor.storage.base import (
+    EventStore,
+    MetricSample,
+    QueueLease,
+    QueueStats,
+    QueueStatus,
+    StoreResult,
+)
 from rtmonitor.storage.memory import InMemoryEventStore
 from rtmonitor.storage.sqlalchemy import SqlAlchemyEventStore
 
 __all__ = [
     "EventStore",
     "InMemoryEventStore",
+    "MetricSample",
     "QueueLease",
     "QueueStats",
     "QueueStatus",
